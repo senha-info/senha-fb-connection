7d5663907903684a0ac9af9765046dd1a8ddb793
Bruno Gaspar <bruninhoogaspar@gmail.com>
2026-02-19 14:31:03 -0300

    fix: add correct build where params

--- src/build-where.ts
+++ src/build-where.ts
@@ -1,12 +1,13 @@
-interface Condition {
+interface Condition<T> {
+  value: T;
   apply: boolean;
-  text: string;
+  text: (value: NonNullable<T>) => string;
 }
 
-export function buildWhere(conditions: Condition[]): string {
+export function buildWhere<T>(conditions: Condition<T>[]): string {
   const where = conditions
-    .filter((condition) => condition.apply)
-    .map((condition) => condition.text)
+    .filter((condition) => condition.apply && condition.value !== undefined)
+    .map((condition) => condition.text(condition.value!))
     .join(" and ");
 
   return where ? `where ${where}` : "";
